27c1f5815cf5ca15d2f945ff781c83ffe34490c0
Holger <holger.steffens@gmail.com>
2024-04-20 18:12:51 +0200

    more debug info

--- simple_app.py
+++ simple_app.py
@@ -8,6 +8,10 @@ st.write("This is a dummy app for testing only!")
 secrets_path = os.path.join(pathlib.Path.home(), "py_app_exchange", "secrets.yaml")
 if not os.path.exists(secrets_path):
     st.write(f"Error: secrets.yaml not found at {secrets_path}!")
+    st.write(f"{os.getcwd()=}")
+    st.write(f"{pathlib.Path.home()=}")
+    st.write(f"{os.listdir(os.getcwd())=}")
+    st.write(f"{os.listdir(pathlib.Path.home())=}")
 else:    
     with open(secrets_path, "r") as file:
         secrets = YAML().load(file)
