9287a92a34c09f4a13f6adb66be5f08ac6319a51
Holger <holger.steffens@gmail.com>
2024-04-20 18:45:10 +0200

    move to home_dir

--- simple_app.py
+++ simple_app.py
@@ -1,15 +1,16 @@
 import streamlit as st
-import os, pathlib
+import os
 from ruamel.yaml import YAML
 
 st.header("Simple App", divider="green")
 st.write("This is a dummy app for testing only!")
 
-secrets_path = os.path.join(pathlib.Path.home(), "py_app_exchange", "secrets.yaml")
+home_dir = os.path.expanduser("~")
+secrets_path = os.path.join(home_dir, "py_app_exchange", "secrets.yaml")
 if not os.path.exists(secrets_path):
     st.write(f"Error: secrets.yaml not found at {secrets_path}!")
     st.write(f"{os.getcwd()=}")
-    st.write(f"{os.listdir(pathlib.Path(os.getcwd()).parent)=}")
+    st.write(f"{os.listdir(home_dir)=}")
 else:    
     with open(secrets_path, "r") as file:
         secrets = YAML().load(file)
